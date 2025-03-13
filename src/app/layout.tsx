491b7b85852ac5a88af4475ee2974724af9cd5d0
Enzo Panem <epanem@my.yorku.ca>
2025-03-13 16:09:48 -0400

    Setup Metadata API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    "title": "ECHO",
+    "description": "Search and Rescue Robot",
+}
+
 export default function RootLayout({
     children,
   }: {
@@ -5,20 +12,6 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <link rel="icon" type="image/x-icon" href="./public/echo_w.png" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-          <meta name="theme-color" content="#000000"/>
-
-          <meta name="apple-mobile-web-app-capable" content="yes"/>
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
-          <meta name="apple-mobile-web-app-title" content="ECHO"/>
-
-          <link rel="apple-touch-icon" href="./public/echo.png"/>
-          
-          <title>ECHO</title>
-        </head>
         <body>
           <div id="root">{children}</div>
         </body>
