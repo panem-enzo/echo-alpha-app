71aabfaf52204174ef188839bce5a0df25077ca7
Enzo Panem <epanem@my.yorku.ca>
2025-03-17 21:14:54 -0400

    Fixed layout of buttons in landscape ui

--- frontend/src/components/LandscapeUI.tsx
+++ frontend/src/components/LandscapeUI.tsx
@@ -4,41 +4,44 @@ import MicButton from "./MicButton";
 import FlashlightButton from "./FlashlightButton";
 import RotateDisplay from "./RotateButton";
 
-const LandscapeUI: React.FC<{ toggleRotation: () => void }> = ({ toggleRotation }) => {
+const LandscapeUI: React.FC<{ toggleRotation: () => void }> = ({
+  toggleRotation,
+}) => {
   return (
     <div className="relative w-full h-screen bg-black">
       {/* Video Stream */}
       <div className="absolute inset-0">
         <video className="w-full h-full object-cover" autoPlay muted loop>
-          <source src="/video-feed.mp4" type="video/mp4" />
+          <source src="/test_footage.mp4" type="video/mp4" />
         </video>
       </div>
-      
+
       {/* Top Right Status Indicators */}
       <div className="absolute top-2 right-2 flex items-center gap-3">
         <div className="w-3 h-3 bg-red-600 rounded-full animate-pulse"></div>
         <Wifi className="text-white" />
         <User className="text-white" />
       </div>
-      
-      {/* Left Joystick */}
-      <div className="absolute left-4 bottom-8">
-        <Joystick />
-      </div>
 
-      {/* Right Joystick */}
-      <div className="absolute right-4 bottom-8">
+      {/* Centered Vertical Stack (Joysticks + Buttons) */}
+      <div className="absolute left-4 top-1/2 transform -translate-y-1/2 flex flex-col items-center gap-y-12">
         <Joystick />
-      </div>
-
-      {/* Bottom Controls */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-6">
-        <MicButton />
-        <FlashlightButton />
-        <RotateDisplay toggleRotation={toggleRotation} />
+        <div className="flex flex-col items-center py-4 px-2 gap-y-2 bg-black/20 rounded-full">
+        <div className="rotate-90 opacity-100">
+          <MicButton />
+        </div>
+        <div className="rotate-90 opacity-100">
+          <FlashlightButton />
+        </div>
+        <div className="rotate-90 opacity-100">
+          <RotateDisplay toggleRotation={toggleRotation} />
+        </div>
+        </div>
+       
+        <Joystick />
       </div>
     </div>
   );
-}
+};
 
 export default LandscapeUI;
